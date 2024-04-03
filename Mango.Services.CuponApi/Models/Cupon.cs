349beb3b4f06f4de0d98a5d4f5fbb0457e11ee19
Rina Bedini <rina@e-soft.al>
2024-04-03 12:29:25 +0200

    Add project files.

---
+++ Mango.Services.CuponApi/Models/Cupon.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.CuponApi.Models
+{
+    public class Cupon
+    {
+        public int CuponId { get; set; }
+        public string CuponCode { get; set; }
+        public string DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
